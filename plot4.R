76b92f5fc48e33a484d6eff1592bd0c5928d05cb
seblun <seblun@gmail.com>
2014-06-07 16:49:40 +0100

    Ready

--- plot4.R
+++ plot4.R
@@ -15,23 +15,23 @@ data[,9] <- as.numeric(as.character(data[,9])) # Sub metering 2
 png(file="plot4.png", width=480, height=480, units="px")
 par(mfrow=c(2,2))
 
-plot(data$datetime,data$Global_active_power, xlab="", 
-     ylab="Global Active Power (kilowatts)", type="n")
+plot(data$datetime,data$Global_active_power, 
+     xlab="", ylab="Global Active Power", type="n")
 lines(data$datetime,data$Global_active_power)
 
 plot(data$datetime,data$Voltage,xlab="datetime", ylab="Voltage", type="n")
 lines(data$datetime,data$Voltage)
 
-plot(data$datetime,data$Sub_metering_1,xlab="", 
-     ylab="Energy sub metering", type="n")
+plot(data$datetime,data$Sub_metering_1,
+     xlab="", ylab="Energy sub metering", type="n")
 lines(data$datetime,data$Sub_metering_1, col="black")
 lines(data$datetime,data$Sub_metering_2, col="red")
 lines(data$datetime,data$Sub_metering_3, col="blue")
 legend("topright",
        legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
-       col=c("black","red","blue"), lty=1)
+       col=c("black","red","blue"), lty=1, bty="n")
 
-plot(data$datetime,data$Global_reactive_power,
+plot(data$datetime, data$Global_reactive_power, 
      xlab="datetime", ylab="Global_reactive_power", type="n")
 lines(data$datetime,data$Global_reactive_power)
 
